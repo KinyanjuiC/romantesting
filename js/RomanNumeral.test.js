98780264114b7891c1dc0f0e41f64f1ede3b2abd
KinyanjuiC <collinskimani6@gmail.com>
2025-03-20 15:17:59 +0300

    updated test file with commentary

--- js/RomanNumeral.test.js
+++ js/RomanNumeral.test.js
@@ -1,5 +1,6 @@
 const romanToInt = require('./RomanNumeral');
 
+// Test conversion of single Roman numeral characters
 test('Single letters', () => {
     expect(romanToInt('I')).toBe(1);
     expect(romanToInt('V')).toBe(5);
@@ -10,31 +11,38 @@ test('Single letters', () => {
     expect(romanToInt('M')).toBe(1000);
 });
 
+// Test conversion of multiple Roman numeral characters
 test('Multiple letters', () => {
     expect(romanToInt('XI')).toBe(11);
 });
 
+// Test conversion of Roman numerals with subtractive notation
 test('Subtractive notation', () => {
     expect(romanToInt('IV')).toBe(4);
     expect(romanToInt('XIV')).toBe(14);
 });
 
+// Test conversion of repeated Roman numeral characters
 test('Repetition', () => {
     expect(romanToInt('II')).toBe(2);
 });
 
+// Test conversion of an invalid Roman numeral character
 test('Invalid letter', () => {
     expect(() => romanToInt('Z')).toThrow('Invalid Roman numeral character: Z');
 });
 
+// Test conversion of a string with both valid and invalid Roman numeral characters
 test('Invalid and valid letter', () => {
     expect(() => romanToInt('XIZ')).toThrow('Invalid Roman numeral character: Z');
 });
 
+// Test conversion of a string with invalid Roman numeral repetition
 test('Invalid repetition', () => {
     expect(() => romanToInt('VV')).toThrow('Invalid repetition of Roman numeral character: V');
 });
 
+// Test conversion of a null string
 test('Null input', () => {
     expect(() => romanToInt(null)).toThrow('Input string is null');
 });
